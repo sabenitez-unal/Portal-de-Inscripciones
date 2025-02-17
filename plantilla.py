581751af447c9dc852985299b9d5a297e82be80c
Samuel Benitez Culma <sabenitez@unal.edu.co>
2025-02-17 13:27:55 -0500

    Se define la carga del departamento y de la ciudad dentro de la función

--- plantilla.py
+++ plantilla.py
@@ -285,7 +285,15 @@ class Participantes:
 
     def carga_ciudad_dpto(self):
         '''Carga los datos en los campos de ciudad y departamento'''
-        pass
+        
+        # Cargando los datos de la DB para el departamento según la ciudad dada
+        query = 'SELECT Nombre_Departamento FROM t_ciudades WHERE Nombre_Ciudad = ?'
+        parametro = (self.treeDatos.item(self.treeDatos.selection())['values'][1],)
+        db_rows = self.run_Query(query, parametro)
+        self.entryDpto.set(db_rows[0][0])
+
+        # Cargando la ciudad
+        self.entryCiudad.set(self.treeDatos.item(self.treeDatos.selection())['values'][1])
               
     def limpia_Campos(self):
         '''Limpia los campos de entrada de los datos'''
